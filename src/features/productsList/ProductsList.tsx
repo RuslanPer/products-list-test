804f3d4d7092a401138f5b859c8e5bd9323bc96b
Ruslan Pershin <ruslan.pershin98@gmail.com>
2022-10-18 00:21:06 +0300

    feat: database

--- src/features/productsList/ProductsList.tsx
+++ src/features/productsList/ProductsList.tsx
@@ -1,37 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState, ChangeEvent } from 'react'
 import {
 	EditOutlined,
 	EllipsisOutlined,
 	SettingOutlined,
 } from '@ant-design/icons'
 import { Avatar, Card, Skeleton, Switch } from 'antd'
+import { db } from '../../firebase'
+import { onValue, ref, set } from 'firebase/database'
 
 const { Meta } = Card
 
 export const ProductsList = () => {
+	const [products, setProducts] = useState<any>([])
+
+	useEffect(() => {
+		onValue(ref(db), snapshot => {
+			const data = snapshot.val()
+			if (data !== null) {
+				Object.values(data.products.items).map(pr => {
+					setProducts((oldArray: any) => [...oldArray, pr])
+				})
+			}
+		})
+	}, [])
+
+	console.log(products)
+
 	return (
 		<div>
-			<Card
-				style={{ width: 300, marginTop: 16 }}
-				loading={false}
-				cover={
-					<img
-						alt='example'
-						src='https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png'
+			{products.map((pr: any) => (
+				<Card
+					style={{ width: 300, marginTop: 16 }}
+					loading={false}
+					cover={<img alt='example' src={pr.imageURL} />}
+					actions={[
+						<SettingOutlined key='setting' />,
+						<EditOutlined key='edit' />,
+						<EllipsisOutlined key='ellipsis' />,
+					]}
+				>
+					<Meta
+						avatar={<Avatar src='https://joeschmoe.io/api/v1/random' />}
+						title={pr.title}
+						description={pr.description}
 					/>
-				}
-				actions={[
-					<SettingOutlined key='setting' />,
-					<EditOutlined key='edit' />,
-					<EllipsisOutlined key='ellipsis' />,
-				]}
-			>
-				<Meta
-					avatar={<Avatar src='https://joeschmoe.io/api/v1/random' />}
-					title='Card title'
-					description='This is the description'
-				/>
-			</Card>
+				</Card>
+			))}
 		</div>
 	)
 }
